c5cd0b304f78201de36116b00eb6b57282110f76
IvetaVrbova <Iveta.vrbova.iv@gmail.com>
2021-05-04 11:07:01 +0200

    pridani nove vizitky

--- src/main/java/cz/czechitas/java2webapps/ukol3/controller/VizitkaController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol3/controller/VizitkaController.java
@@ -1,11 +1,12 @@
 package cz.czechitas.java2webapps.ukol3.controller;
 
-import cz.czechitas.java2webapps.ukol3.entity.Person;
+import cz.czechitas.java2webapps.ukol3.entity.Vizitka;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -13,30 +14,100 @@ import java.util.List;
  */
 @Controller
 public class VizitkaController {
-  private final List<Person> persons;
+    private final List<Vizitka> seznamVizitek;
+    private int vybranaVizitkaId;
 
-  public VizitkaController() {
-    persons = Arrays.asList(
-            new Person("Barbora Kaštylová", "Škrobárenská 511/3", "61700 Brno-jih", "barbora.kastylova@czechitas.cz", "+420 603 403 877", "www.czechitas.cz", "Czechitas"),
-            new Person("Pavlína Vencovská", "Škrobárenská 511/3", "61700 Brno-jih", "pavlina.vencovska@czechitas.cz", null, "www.czechitas.cz", "Czechitas"),
-            new Person("Lenka Růžičková", "Lipová 1789/9", "37005 České Budějovice", "lenka.ruzickova@czechitas.cz", null, "www.czechitas.cz", "Czechitas"),
-            new Person("Michaela Hejnová", "Václavské náměstí 837/11", "11000 Praha 1", "michaela.hejnova@czechitas.cz", "+420 724 341 694", null, "Czechitas"),
-            new Person("Ilona Tandlerová", "Jungmannova 3", "77900 Olomouc", "ilona.tandlerova@czechitas.cz", "+420 734 172 403","www.czechitas.cz", "Czechitas")
-    );
-  }
+    public VizitkaController() {
+        seznamVizitek = new ArrayList<>();
+        seznamVizitek.add(
+                new Vizitka(
+                        "Barbora Kaštylová",
+                        "Czechitas z. s.",
+                        "Škrobárenská 511/3",
+                        "61700 Brno-jih",
+                        "barbora.kastylova@czechitas.cz",
+                        "+420 603 403 877",
+                        "www.czechitas.cz"
+                )
+        );
+        seznamVizitek.add(
+                new Vizitka(
+                        "Pavlína Vencovská",
+                        "Czechitas z. s.",
+                        "Škrobárenská 511/3",
+                        "61700 Brno",
+                        "pavlina.vencovska@czechitas.cz",
+                        null,
+                        "www.czechitas.cz"
+                )
+        );
+        seznamVizitek.add(
+                new Vizitka(
+                        "Lenka Růžičková",
+                        "Czechitas z. s.",
+                        "Lipová 1789/9",
+                        "37005 České Budějovice",
+                        "lenka.ruzickova@czechitas.cz",
+                        null,
+                        "www.czechitas.cz"
+                )
+        );
+        seznamVizitek.add(
+                new Vizitka(
+                        "Michaela Hejnová",
+                        "Czechitas z. s.",
+                        "Václavské náměstí 837/11",
+                        "11000 Praha 1",
+                        "michaela.hejnova@czechitas.cz",
+                        "+420 724 341 694",
+                        "www.czechitas.cz"
+                )
+        );
+        seznamVizitek.add(
+                new Vizitka(
+                        "Ilona Tandlerová",
+                        "Czechitas z. s.",
+                        "Jungmannova 3",
+                        "77900 Olomouc",
+                        "ilona.tandlerova@czechitas.cz",
+                        "+420 734 172 403",
+                        "www.czechitas.cz"
+                )
+        );
 
-  @GetMapping("/")
-  public ModelAndView seznam() {
-    ModelAndView modelAndView = new ModelAndView("seznam");
-    modelAndView.addObject("persons", persons);
-    return modelAndView;
-  }
+    }
 
-  @GetMapping("/detail")
-  public ModelAndView detail(int id) {
-    ModelAndView modelAndView = new ModelAndView("detail");
-    modelAndView.addObject("person", persons.get(id));
-    return modelAndView;
+    @GetMapping("/")
+    public ModelAndView seznam() {
+        ModelAndView result = new ModelAndView("seznam");
+        result.addObject("seznam", seznamVizitek);
+        return result;
+    }
 
-  }
+    @GetMapping(path = "/detail", params = "id")
+    public ModelAndView detail(int id) {
+        ModelAndView result = new ModelAndView("detail");
+        result.addObject("vizitka", seznamVizitek.get(id));
+        result.addObject("id", id);
+
+        return result;
+    }
+
+    @GetMapping(path = "/nova")
+    public ModelAndView nova() {
+        ModelAndView result = new ModelAndView("nova");
+        return result;
+    }
+
+    @PostMapping(value = "/nova", params = {"jmeno", "firma", "ulice", "obecPsc", "email", "telefon", "web"})
+    public String nova(Vizitka vizitka) {
+        seznamVizitek.add(vizitka);
+        return "redirect:/";
+    }
+
+    @PostMapping(value = "/detail", params = {"id"})
+    public String delete(int id) {
+        seznamVizitek.remove(id);
+        return "redirect:/";
+    }
 }
